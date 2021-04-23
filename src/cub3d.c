1a4cd1fa2f329fe80b7a7f37d5f7602f9814778d
gmarcha <gmarcha@github.com>
2021-04-23 18:05:39 +0200

    up

--- src/cub3d.c
+++ src/cub3d.c
@@ -1,10 +1,18 @@
 #include "cub3d.h"
 
+void				mlx_draw_pixel(t_img *mlx_img, int x, int y, int color)
+{
+	char			*target;
+
+	target = mlx_img->image + (x * (mlx_img->bpp / 8) + y * mlx_img->size_line);
+	*target = color;
+}
+
 int					main(void)
 {
 	void			*mlx;
 	void			*mlx_win;
-	t_img			*img;
+	void			*mlx_img;
 
 	mlx = mlx_init();
 	if (mlx == 0)
@@ -18,12 +26,14 @@ int					main(void)
 		ft_putendl_fd("error: can't create a new window", 2);
 		return (1);
 	}
-	img = mlx_new_image(mlx, 1920, 1080);
-	if (img == 0)
+	mlx_img = mlx_new_image(mlx, 1920, 1080);
+	if (mlx_img == 0)
 	{
 		ft_putendl_fd("error: can't create a new image", 2);
 		return (1);
 	}
+	mlx_draw_pixel(mlx_img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, mlx_img, 0, 0);
 	mlx_loop(mlx);
 	ft_putendl_fd("Hello World!", 1);
 	return (0);
