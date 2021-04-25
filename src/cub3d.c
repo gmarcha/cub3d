28289ea1d4d5daf3fb9ee3bb380e3daf8113e053
gmarcha <gmarcha@github.com>
2021-04-25 20:28:00 +0200

    add wall textures

--- src/cub3d.c
+++ src/cub3d.c
@@ -99,6 +99,8 @@ t_root				*parse_texture(t_root *root, char **buf, t_img *img)
 {
 	char			*path;
 	int				len;
+	int				width;
+	int				height;
 
 	if (img)
 		return (destroy(root, 2, "error: multiple texture definition"));
@@ -108,10 +110,12 @@ t_root				*parse_texture(t_root *root, char **buf, t_img *img)
 	path = ft_substr(*buf, 0, len);
 	if (path == 0)
 		return (destroy(root, 2, "error: no texture file"));
-	img = mlx_xpm_file_to_image(root->mlx, path, &img->width, &img->height);
+	img = mlx_xpm_file_to_image(root->mlx, path, &width, &height);
 	free(path);
 	if (img == 0)
 		return (destroy(root, 2, "error: can't load texture"));
+	img->width = width;
+	img->height = height;
 	if (**buf != '\n')
 		return (destroy(root, 2, "error: invalid texture path"));
 	return (root);
