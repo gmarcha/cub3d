d7abec3399a6e83a2373030b8eb665076a9a3f33
gmarcha <gmarcha@github.com>
2021-04-25 19:44:56 +0200

    up

--- src/cub3d.c
+++ src/cub3d.c
@@ -171,6 +171,7 @@ t_root				*root_init(void)
 t_root				*parse_scene(char *buf)
 {
 	t_root			*root;
+	int				i;
 
 	root = root_init();
 	if (root == 0)
@@ -233,6 +234,14 @@ t_root				*parse_scene(char *buf)
 				return (destroy(root, 2, "error: wrong identifier"));
 		}
 	}
+	if (root->window_width == -1 || root->window_height == -1)
+		return (destroy(root, 2, "error: resolution is missing"));
+	i = 0;
+	while (i < 4)
+		if (root->walls_texture[i++] == 0 || root->sprite_texture == 0)
+			return (destroy(root, 2, "error: texture is missing"));
+	if (root->floor_color == -1 || root->ceil_color == -1)
+		return (destroy(root, 2, "error: floor or ceil color is missing"));
 	return (root);
 }
 
