beedac6482a04cb360385d666afa4e66036d816f
gmarcha <gmarcha@github.com>
2021-04-26 19:10:10 +0200

    up

--- src/cub3d.c
+++ src/cub3d.c
@@ -69,7 +69,16 @@ t_ray	*ray_init(t_root *root, int i)
 
 t_ray	*ray_texture(t_root *root, t_ray *ray)
 {
-	(void)root;
+	if (ray->card % 2)
+		ray->wall_coord = root->pos_y + ray->dir_y * ray->wall_dist;
+	else
+		ray->wall_coord = root->pos_x + ray->dir_x * ray->wall_dist;
+	ray->wall_coord -= floor(ray->wall_coord);
+	ray->text_x = (int)(ray->wall_coord * (double)root->walls_texture[0]->width);
+	if ((ray->card % 2 && ray->dir_x > 0) || (ray->card % 2 == 0 && ray->dir_y < 0))
+		ray->text_x = (double)root->walls_texture[0]->width - ray->text_x - 1;
+	ray->text_step = 1.0 * root->walls_texture[0]->height / ray->wall_height;
+	ray->text_pos = (ray->wall_start - root->height / 2 + ray->wall_height / 2) * ray->text_step;
 	return (ray);
 }
 
