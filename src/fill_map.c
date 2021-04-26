352d1752bfa1ddb6ca5b935e2e03df76393b33d0
gmarcha <gmarcha@github.com>
2021-04-26 17:15:47 +0200

    up

--- src/fill_map.c
+++ src/fill_map.c
@@ -4,16 +4,16 @@ int	fill_map(t_root *root, int i, int j, char c)
 {
 	if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
 	{
-		root->pos_x = i;
-		root->pos_y = j;
+		root->pos_x = i + 0.5;
+		root->pos_y = j + 0.5;
 		if (c == 'S')
-			root->dir_x = -1;
-		if (c == 'N')
 			root->dir_x = 1;
+		if (c == 'N')
+			root->dir_x = -1;
 		if (c == 'E')
-			root->dir_y = 1;
-		if (c == 'W')
 			root->dir_y = -1;
+		if (c == 'W')
+			root->dir_y = 1;
 	}
 	if (c == '0')
 		root->map[i][j] = 0;
