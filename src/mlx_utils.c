21e2719620c77319acfa4dc751783eeef20fd4b4
gmarcha <gmarcha@github.com>
2021-04-26 15:56:20 +0200

    up

--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -4,7 +4,6 @@ void	mlx_draw_pixel(t_img *mlx_img, int x, int y, int color)
 {
 	char			*target;
 
-	//printf("%d\n", );
 	target = mlx_img->data + (x * mlx_img->bpp / 8 + y * mlx_img->size_line);
 	*(unsigned int *)target = color;
 }
