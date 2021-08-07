62b8d45bc67157409616c38f234182e485db465b
gmarcha <gmarcha@github.com>
2021-08-06 21:40:41 -0400

    update header: test speed values

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -16,8 +16,8 @@
 # define SCREEN_WIDTH	1920
 # define SCREEN_HEIGHT	1080
 
-# define MOVE_SPEED		1
-# define ROTATION_SPEED	1
+# define MOVE_SPEED		0.5
+# define ROTATION_SPEED	0.5
 
 typedef struct s_root
 {
