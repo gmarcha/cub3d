6b63226150224a10378a4d2edfd8f33af0669803
gmarcha <gmarcha@github.com>
2021-08-18 14:53:48 +0200

    update src: update orientation initialization for player

--- src/input/fill_map.c
+++ src/input/fill_map.c
@@ -7,13 +7,29 @@ int	fill_map(t_root *root, int i, int j, char c)
 		root->pos_x = i + 0.5;
 		root->pos_y = j + 0.5;
 		if (c == 'S')
+		{
+			root->player_orientation = SOUTH;
 			root->dir_x = 1;
+			root->plane_y = 0.8;
+		}
 		if (c == 'N')
+		{
+			root->player_orientation = NORTH;
 			root->dir_x = -1;
+			root->plane_y = 0.8;
+		}
 		if (c == 'E')
+		{
+			root->player_orientation = EAST;
 			root->dir_y = -1;
+			root->plane_x = 0.8;
+		}
 		if (c == 'W')
+		{
+			root->player_orientation = WEST;
 			root->dir_y = 1;
+			root->plane_x = 0.8;
+		}
 	}
 	if (c == '0')
 		root->map[i][j] = 0;
