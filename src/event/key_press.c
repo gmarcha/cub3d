3f027d6bac2c84708c9cae6ceffddfe1495532cf
gmarcha <gmarcha@github.com>
2021-08-18 14:51:53 +0200

    update src: correction redraw on event

--- src/event/key_press.c
+++ src/event/key_press.c
@@ -8,17 +8,34 @@ int	key_press(int keycode, t_root *root)
 		exit(0);
 	}
 	if (keycode == 119)
+	{
 		root->move_up = 1;
+		update(root);
+	}
 	else if (keycode == 115)
+	{
 		root->move_down = 1;
+		update(root);
+	}
 	else if (keycode == 97)
+	{
 		root->move_left = 1;
+		update(root);
+	}
 	else if (keycode == 100)
+	{
 		root->move_right = 1;
+		update(root);
+	}
 	else if (keycode == 65361)
+	{
 		root->rotate_left = 1;
+		update(root);
+	}
 	else if (keycode == 65363)
+	{
 		root->rotate_right = 1;
-	update(root);
+		update(root);
+	}
 	return (1);
 }
